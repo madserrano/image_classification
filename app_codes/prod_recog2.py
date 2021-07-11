af553c51ac4d1a8156b300a4b79d20882446df61
iamAjayDahiya <84476227+iamAjayDahiya@users.noreply.github.com>
2021-07-10 20:23:22 -0400

    Add files via upload

--- app_codes/prod_recog2.py
+++ app_codes/prod_recog2.py
@@ -1,7 +1,10 @@
-## PRE REQUISITE:####################################
+## PRE REQUISITE:#########################################
 # In conda or cmd: pip install streamlit
 # To run the py code: streamlit run prod_recog.py
-#####################################################
+# Changes:
+# 10-July-2021 - Line 109 changed subcategory_link to link
+#              - Cleaned uneccessary lines 
+###########################################################
 
 ## Import Libraries
 import streamlit as st
@@ -10,6 +13,7 @@ import numpy as np
 from skimage import color
 from skimage import io
 import pandas as pd
+import tensorflow as tf
 
 
 def model_id_find(radio_sel):
@@ -44,26 +48,19 @@ def category_pos_fetch(model_out,vmax):
 # loading datasource for link fetching
 ds = pd.read_csv('datasource.csv',index_col=0)
 
-# Load the trained model
-import tensorflow as tf
-
 # loading all model files for faster access, switch to mod_select function if model size too high and comment below lines
 model_0 = tf.keras.models.load_model('PR_model_0.h5')
-model_1 = tf.keras.models.load_model('PR_model_1.h5')
-model_2 = tf.keras.models.load_model('PR_model_2.h5')
-model_3 = tf.keras.models.load_model('PR_model_3.h5')
+#model_1 = tf.keras.models.load_model('PR_model_1.h5')
+#model_2 = tf.keras.models.load_model('PR_model_2.h5')
+#model_3 = tf.keras.models.load_model('PR_model_3.h5')
 model_4 = tf.keras.models.load_model('PR_model_4.h5')
-
-
 
 st.image("header.png")
 
-
-
 ## radio button for categories
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-model_selction_button = st.radio("Select category",('Automotive','Tools & Hardware','Home & Pets','Sports & Recreation','Outdoor Living'))
-src="https://www.canadiantire.ca/en/hot-deals.html?adlocation=HP_ASPOT_CanadaDaySuperSale_21326"
+model_selection_button = st.radio("Select category",('Automotive','Tools & Hardware','Home & Pets','Sports & Recreation','Outdoor Living'))
+#src="https://www.canadiantire.ca/en/hot-deals.html?adlocation=HP_ASPOT_CanadaDaySuperSale_21326"
 
 ## File upload button
 file = st.file_uploader(" ", type=["jpg", "png"])
@@ -80,7 +77,7 @@ if file is None:
     st.text("Please upload an image file")
 else:
     with col1:
-        model_id= model_id_find(model_selction_button)
+        model_id= model_id_find(model_selection_button)
         # trick to not use if loop while selecting model file as per model id, comment when using mod_select function
         name = 'model'
         exec("%s = %s" % (name, name + '_' + str(model_id)))
@@ -91,13 +88,22 @@ else:
         img = io.imread(file)
         st.image(img, width=300)
         im_gray = color.rgb2gray(img) # converting to grayscale
+
+        # # change dimension as per train model, uncomment when every model on same dimensions
+        # dim_w = 90
+        # dim_h = 90
+        # img = cv2.resize(im_gray, (dim_w, dim_h))
+        # img = np.array(img).reshape(-1, dim_w, dim_h, 1)
+
+        # if else case for outer section 90x90 model file
         if model_id==4:
-            img = cv2.resize(im_gray, (90, 90))  # converting to IMG_SIZE 90,90
+            img = cv2.resize(im_gray, (90, 90))  
             img = np.array(img).reshape(-1, 90, 90, 1)
 
         else:
-            img = cv2.resize(im_gray, (28, 28)) # converting to IMG_SIZE 90,90
+            img = cv2.resize(im_gray, (28, 28)) 
             img = np.array(img).reshape(-1, 28, 28, 1)
+
         prediction = model.predict(img)
         # next line will delete model to save ram
         # del model
@@ -112,48 +118,13 @@ else:
 
         # umcomment below 2 lines when loading updated datasource file, with correct id and position
         category_name = ds.loc[((ds.major_category_id == model_id) & (ds.sub_category_id == category_id)), 'Category'].values[0]
-        category_link = ds.loc[((ds.major_category_id == model_id) & (ds.sub_category_id == category_id)), 'sub_category_link'].values[0]
-
-        # delete below 2 line when loading correct ddatsource file
-        # category_name = ds.loc[((ds.Category == 'TRAILERS & TOWING ACCESSORIES') & (ds.product_name == 'ATV, MOTORCYCLES, SNOWMOBILE PARTS & ACCESSORIES')), 'major_category_name'].values[0]
-        # category_link = ds.loc[((ds.Category == 'TRAILERS & TOWING ACCESSORIES') & (ds.product_name == 'ATV, MOTORCYCLES, SNOWMOBILE PARTS & ACCESSORIES')), 'major_category_link'].values[0]
+        category_link = ds.loc[((ds.major_category_id == model_id) & (ds.sub_category_id == category_id)), 'link'].values[0]
 
         st.markdown(str("__"+category_name+"__"))
-        st.write("Confidence: ", max)
-        ctlink = '[=[Open On CT Website]=]({})'.format(category_link)
+        st.write(round(max * 100,2),"%"," Match")
+        ctlink = '[Open on Canadian Tire website]({})'.format(category_link)
         # st.button(ctlink)# cant find documentation for button actions# still in development
         st.markdown(ctlink, unsafe_allow_html=True)
-
-        # if prediction[0:,0] == max :
-        #     st.markdown("__FLOOR MATS & LINERS__")
-        #     st.write("Confidence: ", max)
-        #     st.write("Click this [link](https://www.canadiantire.ca/en/search-results.html?q=FLOOR%20MATS%20%26%20LINERS) to view Floor Mats and Liners products")
-        # elif prediction[0:,1] == max :
-        #     st.markdown("__BRAKE TOOLS__")
-        #     st.write("Confidence: ", max)
-        #     st.write("Click this [link](https://www.canadiantire.ca/en/automotive/loan-a-tool/ac-brake-tools.html) to view Brake Tools products")
-        # elif prediction[0:,2] == max :
-        #     st.markdown("__WHEEL__")
-        #     st.write("Confidence: ", max)
-        #     st.write("Click this [link](https://www.canadiantire.ca/en/automotive/trailers-towing-accessories/trailer-tires-wheels-tubes.html) to view Wheel products")
-        # elif prediction[0:,3] == max :
-        #     st.markdown("__TIRE__")
-        #     st.write("Confidence: ", max)
-        #     st.write("Click this [link](https://www.canadiantire.ca/en/automotive/tires-wheels/tires.html) to view Tire products")
-        # elif prediction[0:,4] == max:
-        #     st.markdown("__MUFFLER__")
-        #     st.write("Confidence: ", max)
-        #     st.write("Click this [link](https://www.canadiantire.ca/en/automotive/auto-parts/emission-exhaust-systems/mufflers.html) to view Muffler products")
-        # elif prediction[0:,5] == max:
-        #     st.markdown("__SPORTS BATTERY__")
-        #     st.write("Confidence: ", max)
-        #     st.write("Click this [link](https://www.canadiantire.ca/en/search-results.html?q=SPORTS%20BATTERY) to view Sports Battery products")
-        # else:
-        #     st.write("Unable to detect. Please load another image.")
-
-## iframe for candaian tire app
-st.components.v1.iframe(src, scrolling=True)
-
 
 st.image("footer.png")
 
